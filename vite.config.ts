23659f32bbba9c156cbcc64a2c658ca2f764dd08
KimSonH <kimson199816@gmail.com>
2023-08-17 21:46:13 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  base: '/admin/',
+  server: {
+    port: 3001
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './vitest.setup.ts'
+  }
+})
